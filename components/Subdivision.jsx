5fa4ca7f656be09857ffdc459bf9eec3387f0707
edu-as <edu-as@hotmail.com>
2022-03-17 23:06:17 -0300

    adding background

---
+++ components/Subdivision.jsx
@@ -0,0 +1,5 @@
+import styles from "../styles/Subdivision.module.css";
+
+export default function Subdivision(props) {
+  return <div className={styles.subdivision}></div>;
+}
---
+++ pages/index.jsx
@@ -0,0 +1,20 @@
+import Subdivision from "../components/Subdivision";
+import styles from "../styles/Home.module.css";
+export default function Home() {
+  return (
+    <div
+      className={styles.container}
+      style={{
+        backgroundImage: "url(./img/bg-retro.gif)",
+        backgroundPosition: "center",
+        backgroundSize: "auto",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <Subdivision />
+      <Subdivision />
+      <Subdivision />
+      
+    </div>
+  );
+}
